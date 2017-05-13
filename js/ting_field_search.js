d756039d67d0303840dc00fd88057b5803316321
Mihaela Stavila <ms@inleadmedia.com>
2017-05-13 14:02:10 +0300

    EASYOPAC-470 - Add fix style..

--- js/ting_field_search.js
+++ js/ting_field_search.js
@@ -9,16 +9,13 @@
     $('.page-search-ting .form-item-ting-field-search').insertAfter('.form-wrapper.collapse-processed'); 
   }
 
-//   $('.form-actions').each(function () {
-//     $(this).insertBefore($(this).prev('.form-item-ting-field-search'));
-// });
-if ($('.ding-user-header-block').length) {
-  $(".form-actions.form-wrapper").insertbefore($(".search-radios-inline"));
-// $( "h2" ).insertAfter( $( ".container" ) );
-}
 
   $(document).ready(function() {
     updateSelect();
     changeSelect();
+
+    if ($('.form-item-ting-field-search').length && $('div#search-radios').length == 0) {
+      $("#search-block-form .form-actions.form-wrapper").insertBefore($(".form-item-ting-field-search"));
+    }
   });
 })(jQuery);
