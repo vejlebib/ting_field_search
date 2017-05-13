f03db9bab177cd1fe8d92a796f7d6e56fe21618c
Mihaela Stavila <ms@inleadmedia.com>
2017-05-13 10:43:12 +0300

    EASYOPAC-470 - Add style fix for work with other modules.

--- js/ting_field_search.js
+++ js/ting_field_search.js
@@ -9,6 +9,14 @@
     $('.page-search-ting .form-item-ting-field-search').insertAfter('.form-wrapper.collapse-processed'); 
   }
 
+//   $('.form-actions').each(function () {
+//     $(this).insertBefore($(this).prev('.form-item-ting-field-search'));
+// });
+if ($('.ding-user-header-block').length) {
+  $(".form-actions.form-wrapper").insertbefore($(".search-radios-inline"));
+// $( "h2" ).insertAfter( $( ".container" ) );
+}
+
   $(document).ready(function() {
     updateSelect();
     changeSelect();
