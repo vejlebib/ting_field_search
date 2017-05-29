5051e9291c4e63117754aa076686019a536daa0d
Mihaela Stavila <ms@inleadmedia.com>
2017-05-30 00:01:29 +0300

    EASYOPAC-470-2 - Hot fix for mobile device.

--- js/ting_field_search.js
+++ js/ting_field_search.js
@@ -9,6 +9,10 @@
       // Change select.
       $('.page-search-ting .form-item-ting-field-search', context).insertAfter('.form-wrapper.collapse-processed');
 
+      if (!($('.ding-user-header').length)) {
+        $('.search-field-wrapper .form-actions.form-wrapper', context).insertAfter('.search-field-wrapper .form-item-search-block-form');
+      }
+
       if ($('#search-radios').length) {
         $('.header-inner').attr('id', 'header-inner-field');
       }
